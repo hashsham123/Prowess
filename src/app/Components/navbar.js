273b821b0f124c462e533a3496bccc1c7b95e603
hashsham123 <hashshamkhan8@gmail.com>
2024-04-14 15:27:28 +0530

    working on navbar

---
+++ src/app/Components/navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../ComponentsStyle/navbar.css";
+function navbar() {
+  return (
+    <div className="parent">
+      <div className="firstChild">Hello Everyone!</div>
+    </div>
+  );
+}
+
+export default navbar;
