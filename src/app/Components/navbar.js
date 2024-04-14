7e023954e237245a2249077cc536b2c6d4fa1264
hashsham123 <hashshamkhan8@gmail.com>
2024-04-14 21:19:08 +0530

    navbar completed

--- src/app/Components/navbar.js
+++ src/app/Components/navbar.js
@@ -8,10 +8,35 @@ function navbar() {
         <div className="logo_holder">
           <img className="logo" src="/Designer.png" alt="Prowess Logo" />
         </div>
-        <div className="searchbar_holder"> 
-          <input className="searchbar" type="search" placeholder="Happy Searching!"></input>
+
+        <div className="searchbar_holder">
+          <input
+            className="searchbar"
+            type="search"
+            placeholder="Happy Searching!"
+          ></input>
+          <img className="searchimage" src="/search.png" alt="search--v1" />
         </div>
         {/* <h1 className="">Hello Everyone!</h1> */}
+
+        <div className="register_holder">
+          <div className="login_holder">
+            <button className="login_button">log in</button>
+          </div>
+          <div className="signup_holder">
+            <button className="signup_button">sign up</button>
+          </div>
+        </div>
+
+        <div className="text_holder">
+            <div className="text1">
+            <h4 className="teach">Teach on prowess</h4>
+            </div>
+
+            <div className="text2">
+            <h4 className="learn">Skill Up</h4>
+            </div>
+        </div>
       </div>
     </div>
   );
