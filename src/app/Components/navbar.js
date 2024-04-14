140aeb509aebd342bae2fc81af63c2d44fd8f855
hashsham123 <hashshamkhan8@gmail.com>
2024-04-14 17:24:09 +0530

    changes in the navbar

--- src/app/Components/navbar.js
+++ src/app/Components/navbar.js
@@ -1,9 +1,18 @@
 import React from "react";
 import "../ComponentsStyle/navbar.css";
+// import Image from "../Images";
 function navbar() {
   return (
     <div className="parent">
-      <div className="firstChild">Hello Everyone!</div>
+      <div className="firstChild">
+        <div className="logo_holder">
+          <img className="logo" src="/Designer.png" alt="Prowess Logo" />
+        </div>
+        <div className="searchbar_holder"> 
+          <input className="searchbar" type="search" placeholder="Happy Searching!"></input>
+        </div>
+        {/* <h1 className="">Hello Everyone!</h1> */}
+      </div>
     </div>
   );
 }
